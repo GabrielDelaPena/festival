15006e59e7b4c6270f5606fc6d2d98dffe202d9b
JDogMad <oumaima.zerouali@gmail.com>
2022-05-07 15:46:52 +0200

    delete

--- assets/js/contact.js
+++
@@ -1,4 +0,0 @@
-// Window Onload. Een laad pagina die alle elementen laat laden dankzij een gif (of iets anders?)
-// Je moet wel alles invullen anders kan je niet klikken op "Sign In" en krijg je een alert "alles invullen aub"
-// Nog niet zeker of we iets zullen doen voor de Wachtwoord Vergeten.
-// Als je op "Sign In" klikt dan Admin Page 
--- assets/js/ticket.js
+++
@@ -1,3 +0,0 @@
-// Window Onload. Een laad pagina die alle elementen laat laden dankzij een gif (of iets anders?)
-// Indien het persoon minder dan 18 jaar is dan komt er een error message "Alleen volwassen persoon mag ticket kopen".
-// Alles invullen anders krijg je een allert "Alles invullen aub"
